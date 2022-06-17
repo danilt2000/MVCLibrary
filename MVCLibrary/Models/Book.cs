e94510ade9015224087e1b36afa0dd34dcde8946
Danil Tkachenko <d_tkachenko@utb.cz>
2022-06-17 11:52:55 +0200

    Add project files.

---
+++ MVCLibrary/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCLibrary.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string CallNumber { get; set; }
+
+
+        public string Author { get; set; }
+
+    }
+}
---
+++ MVCLibrary/Models/SeedData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Data;
+
+namespace MVCLibrary.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<LibraryContext>>()))
+            {
+                if (context.Book.Any())
+                {
+                    return;
+                }
+                context.Book.AddRange(
+                    new Book { Title = "Tiny C# Projects", CallNumber = "AXD 2029" },
+                    new Book { Title = "Tiny Android Projects", CallNumber = "AXQ 2229" }
+                    );
+                context.SaveChanges();
+
+            }            
+                    
+
+        }
+
+
+
+    }
+}
